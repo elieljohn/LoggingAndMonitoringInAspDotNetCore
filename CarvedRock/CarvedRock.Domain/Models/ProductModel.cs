d593acfe01b1ed44cbae94311f7292c70408b621
elieljohn <ejmalabar88@gmail.com>
2024-09-01 14:57:36 +0100

    InitialCommit

---
+++ CarvedRock/CarvedRock.Domain/Models/ProductModel.cs
@@ -0,0 +1,10 @@
+namespace CarvedRock.Domain.Models;
+
+public class ProductModel
+{ 
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public double Price { get; set; }
+    public string Category { get; set; } = null!;
+}
