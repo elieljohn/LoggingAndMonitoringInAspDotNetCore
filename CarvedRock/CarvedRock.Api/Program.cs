394e4b0f2281a84b97bef4fe990b976c13ca1ca1
elieljohn <ejmalabar88@gmail.com>
2024-09-01 18:42:44 +0100

    Revert "03 - Initial Commit"
    This reverts commit b2a56916fd8fee38d864c2e6107fee7d6f2661af.

--- CarvedRock/CarvedRock.Api/Program.cs
+++ CarvedRock/CarvedRock.Api/Program.cs
@@ -1,9 +1,9 @@
-using CarvedRock.Data;
 using CarvedRock.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Services
+// Add services to the container.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -11,27 +11,19 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductLogic, ProductLogic>();
 
-builder.Services.AddDbContext<LocalContext>();
-builder.Services.AddScoped<ICarvedRockRepository, CarvedRockRepository>();
-
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<LocalContext>();
-    context.MigrateAndCreateData();
-}
-
-// HTTP request pipeline
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.MapFallback(() => Results.Redirect("/swagger"));
+
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
