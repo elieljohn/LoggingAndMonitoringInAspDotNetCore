39dcaaaefe87e0385f3a8878f514d33387d14619
elieljohn <ejmalabar88@gmail.com>
2024-09-06 11:19:51 +0100

    Using Log Levels
    1. Add a debug log and a warning log on the HttpGet method which gets a single product by ID.
    ProductController.cs: line 34, 40
    
    2. Add a debug log on GetProductById method. which calls the GetProductById method from the repository.
    ProductLogic.cs: line 36
    
    3. Add the Microsoft.Extensions.Logging.Abstractions dotnet package via the ternminal
    
    4. Inject ILogger<CarvedRockRepository> to the CarverRockRepository constructor and create and assign a private field for the logger
    CarvedRockRepository.cs: lune 10-15
    
    5. Add an information logger on the GetProductsAsync method.
    CarvedRockRepository.cs: line 19
    
    6. Add a debug logger with a timer on the GetProductById method in the repository.
    GetProductById.cs: line 36-44

--- CarvedRock/CarvedRock.Data/CarvedRockRepository.cs
+++ CarvedRock/CarvedRock.Data/CarvedRockRepository.cs
@@ -1,18 +1,23 @@
-﻿using CarvedRock.Data.Entities;
+﻿using System.Diagnostics;
+using CarvedRock.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CarvedRock.Data
 {
     public class CarvedRockRepository :ICarvedRockRepository
     {
         private readonly LocalContext _ctx;
+        private readonly ILogger<CarvedRockRepository> _logger;
 
-        public CarvedRockRepository(LocalContext ctx)
+        public CarvedRockRepository(LocalContext ctx, ILogger<CarvedRockRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
         public async Task<List<Product>> GetProductsAsync(string category)
         {
+            _logger.LogInformation("Getting products in repository for {category}", category);
             return await _ctx.Products.Where(p => p.Category == category || category == "all").ToListAsync();
         }
 
@@ -28,7 +33,15 @@ namespace CarvedRock.Data
 
         public Product? GetProductById(int id)
         {
-            return _ctx.Products.Find(id);
+            var timer = new Stopwatch();
+            timer.Start();
+            var product = _ctx.Products.Find(id);
+            timer.Stop();
+
+            _logger.LogDebug("Querrying products for {id} finished in {milliseconds} milliseconds",
+                id, timer.ElapsedMilliseconds);
+
+            return product;
         }
     }
 }
