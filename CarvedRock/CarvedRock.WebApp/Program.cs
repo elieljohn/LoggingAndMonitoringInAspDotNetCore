8475f9f7fc88800adb35e2f2e2177aa9a531a53b
elieljohn <ejmalabar88@gmail.com>
2024-09-08 18:12:49 +0100

    Using the Error Page in a User Interface
    1. The web app will use the exception handler regardless of the app environment.
    CarvedRock.WebApp.Program.cs: line 10-15
    
    2. CarvedRock.WebApp.appsettings.Development.json is removed
    
    3. Add CurrentActivity and TraceId properties on the error page code.
    Error.cshtml.cs: line 12-13
    
    4. Modify the error page OnGet method so that it captures the current activity and the trace ID.
    Error.cshtml.cs: line 27-28
    
    5. Remove the Development Mode verbage on the error page markup.
    Error.cshtml: line 35-44
    
    6. Modify the error page markup to show more properties the activity, as well as the trace ID.
    Error.cshtml: line 14-31
    
    7. Configure the web app console log settings so that its formatter uses json and that it sets IncludeScopes to true.
    CarvedRock.WebApp.appsettings.json: line 7-18

--- CarvedRock/CarvedRock.WebApp/Program.cs
+++ CarvedRock/CarvedRock.WebApp/Program.cs
@@ -7,12 +7,12 @@ builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+// if (!app.Environment.IsDevelopment())
+// {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+// }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
