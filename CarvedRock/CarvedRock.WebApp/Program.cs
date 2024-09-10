46d78e9088934d98fa78c3ba98cec5f88bb8812b
elieljohn <ejmalabar88@gmail.com>
2024-09-10 17:23:12 +0100

    Using HTTP and W3C Logging
    1. Add HTTP logging service to the builder and configure its LoggingFields, MediaTypeOptions, RequestBodyLogLimit, and RepsonseBodyLogLimit.
    CarvedRock.WebApp.Program.cs: line 4-11
    
    2. Configure pipeline to include HTTP logging by calling UseHttpLogging.
    CarvedRock.WebApp.Program.cs: line 28
    
    3. Add W3C logging service to the builder and configure its options.
    CarvedRock.WebApp.Program.cs: line 12-21
    
    4. Configure the app pipeline to include W3C logging.
    CarvedRock.WebApp.Program.cs: line 29
    
    5. Modify appsettings.json to configure the log level for HTTP logging to information.
    CarvedRock.WebApp.appsettings.json: line 6

--- CarvedRock/CarvedRock.WebApp/Program.cs
+++ CarvedRock/CarvedRock.WebApp/Program.cs
@@ -1,10 +1,33 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging(logging =>
+{
+    // https://bit.ly/aspnetcore6-httplogging
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+});
+var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+builder.Services.AddW3CLogging(opts => {
+    //https://bity.ly/aspnetcore6-w3clogger
+    opts.LoggingFields = W3CLoggingFields.All;
+    opts.FileSizeLimit = 5 * 1024 * 1024;
+    opts.RetainedFileCountLimit = 2;
+    opts.FileName = "CarvedRock-W3C-UI";
+    opts.LogDirectory = Path.Combine(path, "logs");
+    opts.FlushInterval = TimeSpan.FromSeconds(2);
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
+app.UseHttpLogging();
+app.UseW3CLogging();
+
 
 // Configure the HTTP request pipeline.
 // if (!app.Environment.IsDevelopment())
